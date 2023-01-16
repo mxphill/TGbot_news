4a4166779b4199a670f6addb7d1c1c680c3bfb00
Max <122755287+mxphill@users.noreply.github.com>
2023-01-16 12:15:09 +0100

    Update scrapping.py
    Adding comments to the code

--- scrapping.py
+++ scrapping.py
@@ -11,16 +11,16 @@ API_KEY = 'Your_API_KEY'
 url = "Your_Site_URL"
 
 
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"} #user simulation
 
 
 response = requests.get(url, headers=headers)
 print(response)
 bs = BeautifulSoup(response.text, 'lxml')
 
-news_txt = bs.find('h2', 'd-md-none mb-0 pt-1 h5')
+news_txt = bs.find('h2', 'd-md-none mb-0 pt-1 h5') #filter for the news you want
 news = news_txt.text
-print(news)
+print(news) #not necessary, used for comparison
 
 
 
@@ -35,7 +35,7 @@ def send_news(message=None):
         chat_id = 'your_chat_id'
     bot.send_message(chat_id=chat_id, text="Main news -" + news)
 
-to_do = schedule.every().day.at("09:00").do(send_news)
+to_do = schedule.every().day.at("09:00").do(send_news) #еime when me need getting a news
 
 
 while True:
